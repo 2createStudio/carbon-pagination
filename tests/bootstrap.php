33d9125abdf21a9f6c7fdb0f300370450d0629ce
Marin Atanasov <tyxla@abv.bg>
2015-07-10 21:57:08 +0300

    test bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The path to the main plugin directory.
+ */
+define( 'PLUGIN_SRC_PATH', dirname(dirname(__FILE__)) . '/src/' );
+
+/**
+ * Include main plugin file.
+ */
+require_once PLUGIN_SRC_PATH . 'carbon-pagination.php';
