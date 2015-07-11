4639b46c06f52f027680b623c08b937c3a50e8c7
Marin Atanasov <tyxla@abv.bg>
2015-07-11 16:15:49 +0300

    new tests bootstrap, wrapping wround wp tests lib

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,81 @@
 <?php
 /**
- * The path to the main plugin directory.
+ * WooCommerce Unit Tests Bootstrap
+ *
+ * @since 2.2
  */
-define( 'PLUGIN_SRC_PATH', dirname(dirname(__FILE__)) . '/' );
+class Carbon_Pagination_Tests_Bootstrap {
 
-/**
- * Include main plugin file.
- */
-require_once PLUGIN_SRC_PATH . 'carbon-pagination.php';
+	/** 
+	 * The bootstrap instance.
+	 *
+	 * @var Carbon_Pagination_Tests_Bootstrap
+	 */
+	protected static $instance = null;
+
+	/** 
+	 * Directory where wordpress-tests-lib is installed
+	 *
+	 * @var string
+	 */
+	public $wp_tests_dir;
+
+	/** 
+	 * Testing directory.
+	 *
+	 * @var string
+	 */
+	public $tests_dir;
+
+	/** 
+	 * Plugin directory.
+	 *
+	 * @var string
+	 */
+	public $plugin_dir;
+
+	/**
+	 * Setup the unit testing environment
+	 */
+	private function __construct() {
+
+		ini_set( 'display_errors','on' );
+		error_reporting( E_ALL );
+
+		$this->tests_dir    = dirname( __FILE__ );
+		$this->plugin_dir   = dirname( $this->tests_dir );
+		$this->wp_tests_dir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : $this->plugin_dir . '/tmp/wordpress-tests-lib';
+
+		// load test function so tests_add_filter() is available
+		require_once( $this->wp_tests_dir . '/includes/functions.php' );
+
+		// load plugin
+		tests_add_filter( 'muplugins_loaded', array( $this, 'load_plugin' ) );
+
+		// load the WP testing environment
+		require_once( $this->wp_tests_dir . '/includes/bootstrap.php' );
+	}
+
+	/**
+	 * Load the plugin
+	 */
+	public function load_plugin() {
+		require_once( $this->plugin_dir . '/carbon-pagination.php' );
+	}
+
+	/**
+	 * Get the single tests boostrap instance
+	 *
+	 * @return Carbon_Pagination_Tests_Bootstrap
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+}
+
+Carbon_Pagination_Tests_Bootstrap::instance();
