50555bfd111963a7541dce8b857bdce8a1746e29
Marin Atanasov <tyxla@abv.bg>
2015-07-14 16:10:34 +0300

    creating dependancy between  and  upon initialization

--- tests/unit-tests/Carbon_Pagination/CarbonPaginationConstructTest.php
+++ tests/unit-tests/Carbon_Pagination/CarbonPaginationConstructTest.php
@@ -54,4 +54,22 @@ class CarbonPaginationConstructTest extends WP_UnitTestCase {
 		$this->assertSame( 789, $paginationStub->get_total_pages() );
 	}
 
+	public function testAutoPagesGeneration() {
+		$args = array(
+			'total_pages' => 5,
+		);
+		$paginationStub = $this->getMockForAbstractClass( 'Carbon_Pagination', array($args) );
+
+		$this->assertSame( range(1, 5), $paginationStub->get_pages() );
+	}
+
+	public function testAutoTotalPagesSetting() {
+		$args = array(
+			'pages' => range(1, 3)
+		);
+		$paginationStub = $this->getMockForAbstractClass( 'Carbon_Pagination', array($args) );
+
+		$this->assertSame( 3, $paginationStub->get_total_pages() );
+	}
+
 }
