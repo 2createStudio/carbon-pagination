a3aa72e70c4c97e7bd86615234b7c685c7ced96d
Marin Atanasov <tyxla@abv.bg>
2015-08-01 13:44:46 +0300

    tests for Carbon_Pagination_Renderer::__construct()

---
+++ tests/unit-tests/Carbon_Pagination_Renderer/CarbonPaginationRendererConstructTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class CarbonPaginationRendererConstructTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$paginationStub = $this->getMockForAbstractClass( 'Carbon_Pagination' );
+		$this->pagination = $paginationStub;
+
+		$params = array($this->pagination);
+		$collectionStub = $this->getMock('Carbon_Pagination_Collection', null, $params);
+		$this->collection = $collectionStub;
+	}
+
+	public function tearDown() {
+		unset($this->pagination);
+		unset($this->collection);
+	}
+
+	public function testIfCollectionProperlySet() {
+		$params = array($this->collection);
+		$renderer = $this->getMock('Carbon_Pagination_Renderer', null, $params);
+		$this->assertSame( $this->collection, $renderer->get_collection() );
+	}
+
+}
