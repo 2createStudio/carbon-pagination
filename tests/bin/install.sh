985855566685b60be3c7fd17a3c0fa2f4c490fbe
Marin Atanasov <tyxla@abv.bg>
2015-08-07 12:48:26 +0300

    using own db.php in the install script

--- tests/bin/install.sh
+++ tests/bin/install.sh
@@ -37,7 +37,7 @@ install_wp() {
 
 	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
-	curl https://raw.github.com/markoheijnen/wp-mysqli/master/db.php --output $WP_CORE_DIR/wp-content/db.php --silent
+	cp $BASEDIR/tests/misc/db.php $WP_CORE_DIR/wp-content/db.php
 }
 
 install_test_suite() {
