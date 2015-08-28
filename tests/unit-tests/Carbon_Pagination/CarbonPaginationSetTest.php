6971f8cefd465a1bf43a9ae6bb1741102c4f6852
Marin Atanasov <tyxla@abv.bg>
2015-08-28 17:27:33 +0300

    some more tests for Carbon_Pagination::set()

--- tests/unit-tests/Carbon_Pagination/CarbonPaginationSetTest.php
+++ tests/unit-tests/Carbon_Pagination/CarbonPaginationSetTest.php
@@ -24,4 +24,29 @@ class CarbonPaginationSetTest extends WP_UnitTestCase {
 		$this->assertSame( 2, $this->pagination->get_current_page() );
 	}
 
+	public function testPropertySetWithCast() {
+		$args = array(
+			'total_pages' => '5',
+			'current_page' => '2',
+		);
+		$this->pagination->set( $args );
+
+		$this->assertSame( 5, $this->pagination->get_total_pages() );
+		$this->assertSame( 2, $this->pagination->get_current_page() );
+	}
+
+
+	public function testUnexistingProperties() {
+		$this->pagination->set_total_pages(3);
+
+		$args = array(
+			'set_total_pages' => 10,
+			'_total_pages' => 10,
+			'' => 5,
+		);
+		$this->pagination->set( $args );
+
+		$this->assertSame( 3, $this->pagination->get_total_pages() );
+	}
+
 }
