37e86ccfd0f777917135f81163584375a55db9b8
Marin Atanasov <tyxla@abv.bg>
2015-08-28 17:59:41 +0300

    adding tests for Carbon_Pagination_Presenter::__construct()

---
+++ tests/unit-tests/Carbon_Pagination_Presenter/CarbonPaginationPresenterConstructTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @group presenter
+ * @group constructors
+ */
+class CarbonPaginationPresenterConstructTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$paginationStub = $this->getMockForAbstractClass( 'Carbon_Pagination' );
+		$this->pagination = $paginationStub;
+	}
+
+	public function tearDown() {
+		unset( $this->pagination );
+	}
+
+	public function testIfPaginationProperlySet() {
+		$params = array($this->pagination);
+		$presenter = $this->getMock('Carbon_Pagination_Presenter', null, $params);
+		$this->assertSame( $this->pagination, $presenter->get_pagination() );
+	}
+
+}
