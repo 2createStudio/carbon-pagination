33639fb6ba9f0b5b68af22b076a29fe46eb1b8ed
Marin Atanasov <tyxla@abv.bg>
2015-08-28 18:16:26 +0300

    adding tests for Carbon_Pagination_Presenter::render()

---
+++ tests/unit-tests/Carbon_Pagination_Presenter/CarbonPaginationPresenterRenderTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @group presenter
+ */
+class CarbonPaginationPresenterRenderTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$paginationStub = $this->getMockForAbstractClass( 'Carbon_Pagination' );
+		$this->pagination = $paginationStub;
+
+		$params = array($this->pagination, false);
+		$collectionStub = $this->getMock('Carbon_Pagination_Collection', null, $params);
+		$this->collection = $collectionStub;
+
+		$params = array($this->collection);
+		$rendererStub = $this->getMock('Carbon_Pagination_Renderer', null, $params);
+		$this->renderer = $rendererStub;
+
+		$presenter_args = array(
+			$this->pagination,
+		);
+		$presenterStub = $this->getMockForAbstractClass( 'Carbon_Pagination_Presenter', $presenter_args );
+		$this->presenter = $presenterStub;
+	}
+
+	public function tearDown() {
+		unset($this->pagination);
+		unset($this->collection);
+		unset($this->renderer);
+		unset($this->presenter);
+	}
+
+	public function testPresenterRender() {
+		$expected = $this->renderer->render( array(), false );
+		$actual = $this->presenter->render();
+		$this->assertSame( $expected, $actual );
+	}
+
+}
