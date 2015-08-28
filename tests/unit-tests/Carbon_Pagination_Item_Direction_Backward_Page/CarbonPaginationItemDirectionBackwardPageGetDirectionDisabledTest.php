0036f879973dcc63283255eecdda70beaa5325f3
Marin Atanasov <tyxla@abv.bg>
2015-08-29 02:11:18 +0300

    tests for Carbon_Pagination_Item_Direction_Page, Carbon_Pagination_Item_Direction_Backward_Page and Carbon_Pagination_Item_Direction_Forward_Page

---
+++ tests/unit-tests/Carbon_Pagination_Item_Direction_Backward_Page/CarbonPaginationItemDirectionBackwardPageGetDirectionDisabledTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @group item
+ * @group item_direction_page
+ * @group item_direction_page_backward
+ */
+class CarbonPaginationItemDirectionBackwardPageGetDirectionDisabledTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$mock_methods = array( 'get_current_page', 'get_total_pages' );
+		$paginationStub = $this->getMockForAbstractClass('Carbon_Pagination', array(), '', TRUE, TRUE, TRUE, $mock_methods);
+		$this->pagination = $paginationStub;
+		$this->pagination->expects( $this->any() )
+			->method( 'get_total_pages' )
+			->will( $this->returnValue( 10 ) );
+
+		$params = array($this->pagination);
+		$collectionStub = $this->getMock('Carbon_Pagination_Collection', null, $params);
+		$this->collection = $collectionStub;
+
+		$args = array(
+			$this->collection
+		);
+		$this->item = $this->getMockForAbstractClass( 'Carbon_Pagination_Item_Direction_Backward_Page', $args );
+	}
+
+	public function tearDown() {
+		unset( $this->pagination );
+		unset( $this->collection );
+		unset( $this->item );
+	}
+
+	/**
+	 * @covers Carbon_Pagination_Item_Direction_Backward_Page::get_direction_disabled
+	 */
+	public function testOnFirstPage() {
+		$this->pagination->expects( $this->any() )
+			->method( 'get_current_page' )
+			->will( $this->returnValue( 1 ) );
+
+		$this->assertTrue( $this->item->get_direction_disabled() );
+	}
+
+	/**
+	 * @covers Carbon_Pagination_Item_Direction_Backward_Page::get_direction_disabled
+	 */
+	public function testOnNonFirstPage() {
+		$this->pagination->expects( $this->any() )
+			->method( 'get_current_page' )
+			->will( $this->returnValue( 5 ) );
+
+		$this->assertFalse( $this->item->get_direction_disabled() );
+	}
+
+	/**
+	 * @covers Carbon_Pagination_Item_Direction_Backward_Page::get_direction_disabled
+	 */
+	public function testOnLastPage() {
+		$this->pagination->expects( $this->any() )
+			->method( 'get_current_page' )
+			->will( $this->returnValue( 10 ) );
+
+		$this->assertFalse( $this->item->get_direction_disabled() );
+	}
+
+}
---
+++ tests/unit-tests/Carbon_Pagination_Item_Direction_Page/CarbonPaginationItemDirectionPageInitTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @group item
+ * @group item_direction_page
+ */
+class CarbonPaginationItemDirectionPageInitTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$paginationStub = $this->getMockForAbstractClass( 'Carbon_Pagination' );
+		$this->pagination = $paginationStub;
+
+		$params = array($this->pagination);
+		$collectionStub = $this->getMock('Carbon_Pagination_Collection', null, $params);
+		$this->collection = $collectionStub;
+
+		$args = array(
+			$this->collection
+		);
+		$methods = array( 'get_direction_disabled', 'get_collection' );
+		$this->item = $this->getMockForAbstractClass( 'Carbon_Pagination_Item_Direction_Page', $args, '', FALSE, TRUE, TRUE, $methods );
+		$this->item->expects( $this->any() )
+			->method( 'get_collection' )
+			->will( $this->returnValue( $this->collection ) );
+
+	}
+
+	public function tearDown() {
+		unset( $this->pagination );
+		unset( $this->collection );
+		unset( $this->item );
+	}
+
+	/**
+	 * @covers Carbon_Pagination_Item_Direction_Page::init
+	 */
+	public function testWithDirectionDisabled() {
+		$this->item->expects( $this->any() )
+			->method( 'get_direction_disabled' )
+			->will( $this->returnValue( true ) );
+
+		$this->item->init();
+
+		$this->assertFalse( $this->item->get_subitems_collection() );
+	}
+
+	/**
+	 * @covers Carbon_Pagination_Item_Direction_Page::init
+	 */
+	public function testWithDirectionEnabled() {
+		$this->item->expects( $this->any() )
+			->method( 'get_direction_disabled' )
+			->will( $this->returnValue( false ) );
+
+		$this->item->init();
+
+		$this->assertInstanceOf( 'Carbon_Pagination_Collection', $this->item->get_subitems_collection() );
+		$this->assertSame( 1, count( $this->item->get_subitems_collection()->get_items() ) );
+	}
+
+}
