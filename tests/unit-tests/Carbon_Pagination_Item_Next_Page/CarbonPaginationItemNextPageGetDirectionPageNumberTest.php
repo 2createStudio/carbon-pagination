d19952fc17c2e34b52b18708482c6e588e2cfd70
Marin Atanasov <tyxla@abv.bg>
2015-08-30 02:49:39 +0300

    tests for Carbon_Pagination_Item_Next_Page

---
+++ tests/unit-tests/Carbon_Pagination_Item_Next_Page/CarbonPaginationItemNextPageGetDirectionPageNumberTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @group item
+ * @group item_direction_page
+ * @group item_direction_page_forward
+ * @group item_direction_page
+ * @group item_next_page
+ */
+class CarbonPaginationItemNextPageGetDirectionPageNumberTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$mock_methods = array( 'get_current_page' );
+		$paginationStub = $this->getMockForAbstractClass('Carbon_Pagination', array(), '', TRUE, TRUE, TRUE, $mock_methods);
+		$this->pagination = $paginationStub;
+
+		$params = array($this->pagination);
+		$collectionStub = $this->getMock('Carbon_Pagination_Collection', null, $params);
+		$this->collection = $collectionStub;
+
+		$params = array($this->collection);
+		$itemStub = $this->getMock('Carbon_Pagination_Item_Next_Page', null, $params);
+		$this->item = $itemStub;
+
+		$this->current_page = 10;
+		$this->pagination->expects( $this->any() )
+			->method( 'get_current_page' )
+			->will( $this->returnValue( $this->current_page ) );
+	}
+
+	public function tearDown() {
+		unset($this->pagination);
+		unset($this->collection);
+		unset($this->item);
+	}
+
+	/**
+	 * @covers Carbon_Pagination_Item_Next_Page::get_direction_page_number
+	 */
+	public function testGetDirectionPageNumber() {
+		$current_page_idx = $this->current_page - 1;
+		$this->assertSame( $current_page_idx + 1, $this->item->get_direction_page_number() );
+	}
+
+}
