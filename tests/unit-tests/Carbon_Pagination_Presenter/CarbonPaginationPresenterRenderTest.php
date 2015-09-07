72c9934acd7cbd7229bf8bc0792a1eb0d674bc8b
Marin Atanasov <tyxla@abv.bg>
2015-09-07 21:44:06 +0300

    adding new Carbon_Pagination_Presenter::render() tests

--- tests/unit-tests/Carbon_Pagination_Presenter/CarbonPaginationPresenterRenderTest.php
+++ tests/unit-tests/Carbon_Pagination_Presenter/CarbonPaginationPresenterRenderTest.php
@@ -21,6 +21,10 @@ class CarbonPaginationPresenterRenderTest extends WP_UnitTestCase {
 		);
 		$presenterStub = $this->getMockForAbstractClass( 'Carbon_Pagination_Presenter', $presenter_args );
 		$this->presenter = $presenterStub;
+
+		$item_params = array( $this->collection );
+		$this->item1 = $this->getMock( 'Carbon_Pagination_Item', null, $item_params );
+		$this->item2 = $this->getMock( 'Carbon_Pagination_Item', null, $item_params );
 	}
 
 	public function tearDown() {
@@ -28,12 +32,35 @@ class CarbonPaginationPresenterRenderTest extends WP_UnitTestCase {
 		unset($this->collection);
 		unset($this->renderer);
 		unset($this->presenter);
+		unset($this->item1);
+		unset($this->item2);
 	}
 
 	/**
 	 * @covers Carbon_Pagination_Presenter::render
 	 */
-	public function testPresenterRender() {
+	public function testPresenterRenderEmpty() {
+		$expected = $this->renderer->render( array(), false );
+		$actual = $this->presenter->render();
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers Carbon_Pagination_Presenter::render
+	 */
+	public function testPresenterRenderNonEmpty() {
+		$this->collection->expects( $this->any() )
+			->method( 'get_items' )
+			->will( $this->returnValue( array( $this->item1, $this->item2 ) ) );
+
+		$this->item1->expects( $this->any() )
+			->method( 'get_html' )
+			->will( $this->returnValue( array( 'Foo' ) ) );
+
+		$this->item2->expects( $this->any() )
+			->method( 'get_html' )
+			->will( $this->returnValue( array( 'Bar' ) ) );
+
 		$expected = $this->renderer->render( array(), false );
 		$actual = $this->presenter->render();
 		$this->assertSame( $expected, $actual );
