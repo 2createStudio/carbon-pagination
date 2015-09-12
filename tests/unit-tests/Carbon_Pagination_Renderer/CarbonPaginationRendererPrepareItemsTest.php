88373b37495c5649e0a8f71d13a8b749aedba5d1
Marin Atanasov <tyxla@abv.bg>
2015-09-12 13:30:04 +0300

    done tests for Carbon_Pagination_Renderer::prepare_items()

--- tests/unit-tests/Carbon_Pagination_Renderer/CarbonPaginationRendererPrepareItemsTest.php
+++ tests/unit-tests/Carbon_Pagination_Renderer/CarbonPaginationRendererPrepareItemsTest.php
@@ -70,4 +70,76 @@ class CarbonPaginationRendererPrepareItemsTest extends WP_UnitTestCase {
 		$this->assertSame( $expected, $actual );
 	}
 
+	/**
+	 * @covers Carbon_Pagination_Renderer::prepare_items
+	 */
+	public function testWithLibraryClasses() {
+		// mocks of classes that are treated correct items
+		$itemParams = array( $this->collection );
+		$item = $this->getMock('Carbon_Pagination_Item', null, $itemParams);
+		$itemCurrentPageText = $this->getMock('Carbon_Pagination_Item_Current_Page_Text', null, $itemParams);
+		$itemDirectionBackwardPage = $this->getMockForAbstractClass('Carbon_Pagination_Item_Direction_Backward_Page', $itemParams);
+		$itemDirectionForwardPage = $this->getMockForAbstractClass('Carbon_Pagination_Item_Direction_Forward_Page', $itemParams);
+		$itemDirection = $this->getMockForAbstractClass('Carbon_Pagination_Item_Direction_Page', $itemParams);
+		$itemFirstPage = $this->getMock('Carbon_Pagination_Item_First_Page', null, $itemParams);
+		$itemHTML = $this->getMock('Carbon_Pagination_Item_HTML', null, $itemParams);
+		$itemLastPage = $this->getMock('Carbon_Pagination_Item_Last_Page', null, $itemParams);
+		$itemLimiter = $this->getMock('Carbon_Pagination_Item_Limiter', null, $itemParams);
+		$itemNextPage = $this->getMock('Carbon_Pagination_Item_Next_Page', null, $itemParams);
+		$itemNumberLinks = $this->getMock('Carbon_Pagination_Item_Number_Links', null, $itemParams);
+		$itemPage = $this->getMock('Carbon_Pagination_Item_Page', null, $itemParams);
+		$itemPreviousPage = $this->getMock('Carbon_Pagination_Item_Previous_Page', null, $itemParams);
+
+		// mocks of classes that are not treated as correct items
+		$collection = $this->collection;
+		$presenter = $this->getMock('Carbon_Pagination_Presenter', null, array( $this->pagination ));
+		$renderer = $this->renderer;
+		$utilities = $this->getMock('Carbon_Pagination_Utilities');
+		$pagination = $this->getMockForAbstractClass('Carbon_Pagination');
+		$pagination_custom = $this->getMock('Carbon_Pagination_Custom');
+		$pagination_html = $this->pagination;
+
+		$items = array(
+			$item,
+			$itemCurrentPageText,
+			$itemDirectionBackwardPage,
+			$itemDirectionForwardPage,
+			$itemDirection,
+			$itemFirstPage,
+			$itemHTML,
+			$itemLastPage,
+			$itemLimiter,
+			$itemNextPage,
+			$itemNumberLinks,
+			$itemPage,
+			$itemPreviousPage,
+			$collection,
+			$presenter,
+			$renderer,
+			$utilities,
+			$pagination,
+			$pagination_custom,
+			$pagination_html,
+		);
+
+		$expected = array(
+			$item,
+			$itemCurrentPageText,
+			$itemDirectionBackwardPage,
+			$itemDirectionForwardPage,
+			$itemDirection,
+			$itemFirstPage,
+			$itemHTML,
+			$itemLastPage,
+			$itemLimiter,
+			$itemNextPage,
+			$itemNumberLinks,
+			$itemPage,
+			$itemPreviousPage,
+		);
+		$actual = $this->renderer->prepare_items( $items );
+		
+		$this->assertSame( $expected, $actual );
+	}
+
 }
