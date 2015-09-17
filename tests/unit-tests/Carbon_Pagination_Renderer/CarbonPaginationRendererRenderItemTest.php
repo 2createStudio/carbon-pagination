3ecc764737ba8000fb3ac962c7ed36b1b0b27435
Marin Atanasov <tyxla@abv.bg>
2015-09-18 01:14:36 +0300

    wip tests for Carbon_Pagination_Renderer::render_item()

---
+++ tests/unit-tests/Carbon_Pagination_Renderer/CarbonPaginationRendererRenderItemTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @group renderer
+ */
+class CarbonPaginationRendererRenderItemTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		$paginationStub = $this->getMockForAbstractClass( 'Carbon_Pagination_HTML' );
+		$this->pagination = $paginationStub;
+
+		$params = array($this->pagination);
+		$collectionStub = $this->getMock('Carbon_Pagination_Collection', null, $params);
+		$this->collection = $collectionStub;
+
+		$params = array($this->collection);
+		$rendererStub = $this->getMock('Carbon_Pagination_Renderer', null, $params);
+		$this->renderer = $rendererStub;
+
+		$itemParams = array($this->collection);
+		$itemStub = $this->getMockForAbstractClass( 'Carbon_Pagination_Item', $itemParams );
+		$this->item = $itemStub;
+	}
+
+	public function tearDown() {
+		unset($this->pagination);
+		unset($this->collection);
+		unset($this->renderer);
+		unset($this->item);
+	}
+
+}
