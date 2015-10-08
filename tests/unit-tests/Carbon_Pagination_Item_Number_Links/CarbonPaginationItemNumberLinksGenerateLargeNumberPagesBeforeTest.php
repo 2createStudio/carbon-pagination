14e5e96e0d1ca68ab5ee4dfe17455d9b64b246f0
Marin Atanasov <tyxla@abv.bg>
2015-10-08 09:32:36 +0300

    preparing tests for Carbon_Pagination_Item_Number_Links::generate_large_number_pages_before()

---
+++ tests/unit-tests/Carbon_Pagination_Item_Number_Links/CarbonPaginationItemNumberLinksGenerateLargeNumberPagesBeforeTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @group item
+ * @group item_number_links
+ */
+class CarbonPaginationItemNumberLinksGenerateLargeNumberPagesBeforeTest extends WP_UnitTestCase {
+
+	public function setUp() {
+		// mock pagination
+		$mock_methods = array(
+			'get_number_limit',
+			'get_total_pages',
+			'get_current_page',
+			'get_large_page_number_limit', 
+			'get_large_page_number_interval'
+		);
+		$paginationStub = $this->getMockForAbstractClass('Carbon_Pagination_HTML', array(), '', TRUE, TRUE, TRUE, $mock_methods);
+		$this->pagination = $paginationStub;
+
+		// mock collection
+		$params = array($this->pagination, false);
+		$collectionStub = $this->getMock('Carbon_Pagination_Collection', null, $params);
+		$this->collection = $collectionStub;
+
+		// mock item
+		$params = array($this->collection);
+		$itemStub = $this->getMock('Carbon_Pagination_Item_Number_Links', null, $params, '', false);
+		$this->item = $itemStub;
+		$this->item->set_collection($this->collection);
+
+		// setup item's subitems collection manually
+		$params = array( $this->pagination, false );
+		$this->subitems_collection = $this->getMock('Carbon_Pagination_Collection', null, $params );
+		$this->item->set_subitems_collection( $this->subitems_collection );
+
+		// mock total number of pages is always the same - 50
+		$this->pagination->expects( $this->any() )
+			->method( 'get_total_pages' )
+			->will( $this->returnValue( 50 ) );
+	}
+
+	public function tearDown() {
+		unset($this->pagination);
+		unset($this->collection);
+		unset($this->item);
+		unset($this->subitems_collection);
+	}
+
+}
